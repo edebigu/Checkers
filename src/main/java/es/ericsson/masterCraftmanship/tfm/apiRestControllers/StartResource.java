e231fa3b7e9f5b582485f0e26b14025653536488
edebigu <debora.iniguez@gmail.com>
2020-07-23 12:06:08 +0200

    Add delete session in start controller to avoid errors

--- src/main/java/es/ericsson/masterCraftmanship/tfm/apiRestControllers/StartResource.java
+++ src/main/java/es/ericsson/masterCraftmanship/tfm/apiRestControllers/StartResource.java
@@ -2,12 +2,13 @@ package es.ericsson.masterCraftmanship.tfm.apiRestControllers;
 
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
-import org.springframework.http.HttpStatus;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
+
+import es.ericsson.masterCraftmanship.tfm.businessControllers.StartController;
 
 @RestController
 @RequestMapping(StartResource.START)
@@ -19,9 +20,17 @@ public class StartResource {
 	static final String START_LOGIN = "/login";
 	static final String START_REGISTER = "/register";
 	
+	private StartController startController;
+	
+	@Autowired
+	public StartResource(StartController startController) {
+		this.startController = startController;
+	}
+	
 	@GetMapping
 	public ResponseEntity started() {
 		logger.info("Recibido start");
+		this.startController.start();
 		return ResponseEntity.ok().build();
 	}
 	
--- src/main/java/es/ericsson/masterCraftmanship/tfm/businessControllers/StartController.java
+++ src/main/java/es/ericsson/masterCraftmanship/tfm/businessControllers/StartController.java
@@ -1,8 +1,24 @@
 package es.ericsson.masterCraftmanship.tfm.businessControllers;
 
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+
+import es.ericsson.masterCraftmanship.tfm.daos.SessionDao;
 
 @Controller
 public class StartController {
+	
+	private SessionDao sessionDao;
+	
+	@Autowired
+	public StartController (SessionDao sessionDao) {
+		this.sessionDao = sessionDao;
+	}
+
+	
+	public void start() {
+		sessionDao.deleteAll();
+	}
 
 }
