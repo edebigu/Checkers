578937c8cf2fcefbea95345fd8e760398cd0fb85
edebigu <debora.iniguez@gmail.com>
2020-08-03 22:36:14 +0200

    Rename package application

--- src/main/java/es/ericsson/masterCraftmanship/tfm/checkers/Application.java
+++ src/main/java/es/ericsson/masterCraftmanship/tfm/checkers/Application.java
@@ -1,4 +1,4 @@
-package es.ericcsson.masterCraftmanship.tfm.checkers;
+package es.ericsson.masterCraftmanship.tfm.checkers;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
